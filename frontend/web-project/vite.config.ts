a38fc3e8da72ea5c9da434dae2921b68ca9c2e47
André Yermak Naumenko <asyermak@gmail.com>
2024-11-18 23:11:50 +0100

    Bugfix. Style support

--- frontend/web-project/vite.config.ts
+++ frontend/web-project/vite.config.ts
@@ -1,6 +1,12 @@
+import path from "node:path";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
